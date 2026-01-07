93a96876910a5415a01ff43eb2d19ac0cc6b6050
Mizael <mizael.user@outlook.com>
2026-01-07 17:46:49 -0300

    feat: new lib

---
+++ index.js
@@ -0,0 +1,24 @@
+const qrcode = require("qrcode-terminal")
+
+const { Client, LocalAuth } = require('whatsapp-web.js')
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+})
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true })
+})
+
+client.on("ready", () => {
+  console.log("Client is ready!")
+})
+
+client.on("message_create", async (message) => {
+  console.log(message._data.notifyName)
+  if (message.body === "!ping") {
+    client.sendMessage(message.from, "pong")
+  }
+})
+
+client.initialize()
